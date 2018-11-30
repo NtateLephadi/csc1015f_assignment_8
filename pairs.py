9dff9a130662fa4e9d8382b0be90d9e8d0c8dadd
Tumelo Lephadi <lphtum003@myuct.ac.za>
2018-11-30 21:13:47 +0200

    pairs is done

--- pairs.py
+++ pairs.py
@@ -1,10 +1,16 @@
 def pairs(sentence):
-    sentence = sentence.split()
-    count = 0
-    for i in sentence:
-        for j in range(len(i) - 1):
-            if i[j] == i[j + 1]:
-                count += 1
-    return count
+    if sentence == "":
+        return 0
+    elif len(sentence) == 1:
+        return 0
+    elif sentence[0] == sentence[1]:
+        return 1 + pairs(sentence[2:])
+    else:
+        return pairs(sentence[1:])
 
-print(pairs("hello, Salaama"))
+def main():
+    sentence = input("Enter a message:\n")
+    print("Number of pairs: " + str(pairs(sentence)))
+
+if __name__ == '__main__':
+    main()
